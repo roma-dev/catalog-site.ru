3b719118678a665d0012e1fb85012e7d333515e6
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 17:10:07 +0300

    Точка входа для фронтенда думаю уже не будет меняться. Поэтому зафиксирую.

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,20 @@
 <?php
-echo 'home';
-echo $_SERVER['REQUEST_URI'];
+
+use Engine\Catalog;
+use Engine\Request;
+
+// минусуем из строки подстроку frontend
+define('ROOT_DIR', substr(__DIR__, 0, -8 ));
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$config = require_once __DIR__. '/config/config.php';
+$rules = require_once __DIR__. '/config/rules.php';
+
+Catalog::init($config);
+
+Catalog::$app->rules = $rules;
+
+Catalog::$app->request = new Request();
+
+Catalog::$app->goAction();
