00c6e1a746b479ff007091df1cc9fa57c3b38b27
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 13:35:29 +0300

    Реализовал метод render класса View с возможностью использовать макеты.

--- engine/View.php
+++ engine/View.php
@@ -6,10 +6,15 @@ class View {
 
 	public function render($viewName, $arg=[])
 	{
+		//var_dump(Catalog::$app->settings['layoutView']); die;
+		
+		// строим путь до файла макета
+		$layoutPath = ROOT_DIR . Catalog::$app->settings['layoutsPath'] . '/' . Catalog::$app->settings['layoutView'] . '.php';
+		//var_dump($layoutPath); die;
 		// строим путь до файла вида
 		$viewPath = ROOT_DIR . Catalog::$app->settings['viewsPath'] . '/' . $viewName . '.php';
-		// проверяем есть ли такой файл
-		if(!is_file($viewPath)){ Catalog::$app->httpHeader->error(500, 'Отсутствует файл вида!'); }
+		// проверяем существуют ли файлы необходимые для рендеринга
+		if(!is_file($viewPath) && !is_file($layoutPath) ){ Catalog::$app->httpHeader->error(500, 'Отсутствует файл вида!'); }
 		
 		extract($arg);
 		
@@ -17,8 +22,11 @@ class View {
 		ob_implicit_flush(0);
 		
 		try{
+			// файл вида с подстановкой переменых добавляем в переменную контент, которая будет подставляться в файле макета
+			$content = require $viewPath;
 			
-			require $viewPath;
+			// выводим файл макета
+			require $layoutPath;
 			
 		} catch (Exception $ex) {
 
@@ -26,6 +34,7 @@ class View {
 			throw $ex;
 		}
 		
+		// отдаем содержимое буфера в браузер
 		echo ob_get_clean();
 		
 	}
