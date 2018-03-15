2a30986d0adcf5a2ccce9907266b17ec6952f8f4
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 10:23:22 +0300

    Создание метода goAction у класса Catalog.

--- engine/Catalog.php
+++ engine/Catalog.php
@@ -1,20 +1,22 @@
 <?php
 namespace Engine;
 
+use Frontend\controllers;
+
 class Catalog
 {
 	static $app = null;
 	
 	private $services = [];	
 	
-	public static function singleton($config) 
+	public static function init($config) 
 	{
 		if (null === self::$app)
 		{
 			self::$app = new self();
 			self::$app->services['settings'] = $config;
 			self::$app->services['httpHeader'] = new HttpHeader();
-			
+			self::$app->services['rules'] = require_once 'config/rules.php';
 		}
 		return self::$app;
 	}
@@ -32,6 +34,23 @@ class Catalog
 	}
 	
 	
+	public function goAction()
+	{
+		$controller = '\\Frontend\\controllers\\' . self::$app->services['request']->controller;
+		
+		$action = self::$app->services['request']->action;
+		$id = self::$app->services['request']->id;
+		$page = self::$app->services['request']->page;
+		
+		// если по какой-то причине не произойдет вызов экшена то отдаем код сервера "Внутренняя ошибка сервера"
+		if (! call_user_func_array( [ new $controller(),  $action], [ $id, $page ]) ) 
+		{
+			self::$app->httpHeader->error(500);
+		}	
+	}
+
+	
+
 	private function __construct(){}
 	private function __clone(){}
 }
