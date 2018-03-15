71ed15ca34220c89c080a611479eb9ed3e8ca059
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 09:29:00 +0300

    Создание класса Request для получения имя контроллера и экшена.

---
+++ engine/Request.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Engine;
+
+class Request {
+	
+	private $controller;
+	private $action;
+	private $id;
+	private $page;
+	
+	public function __construct() 
+	{
+		if (! $this->checkPath()) { Catalog::$app->httpHeader->error('404'); }
+		
+		$this->controller = Catalog::$app->rules[ $this->getPath() ]['controller'];
+		$this->action = Catalog::$app->rules[ $this->getPath() ]['action'];
+		$this->id = isset($_GET['id']) ? $_GET['id'] : null; 
+		$this->page = isset($_GET['page']) ? $_GET['page'] : null; 
+		
+	}
+
+	/**
+	 * 
+	 * @return alias пример /category
+	 */
+			
+	private function getPath()
+	{
+		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	}
+
+	/**
+	 * 
+	 * @return bool
+	 */
+	
+	private function checkPath()
+	{
+		return isset( Catalog::$app->rules[ $this->getPath() ] ) ? TRUE : FALSE ;
+	}	
+	
+	public function __get($name) 
+	{
+		return $this->$name;
+	}
+}
