d9ea5df2e5fb2bf3acb4d80e7527ecc1bb62b62f
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 21:57:05 +0300

    Добавил переменную controllNamespace в конфиг фронтенда /config/config.php

--- frontend/config/config.php
+++ frontend/config/config.php
@@ -3,5 +3,6 @@ return [
 	'layoutsPath' => 'frontend/views/layouts',
 	'layoutView' => 'DefaultLayout',
 	'viewsPath' => 'frontend/views/default',
+	'controllerNamespace' => '\\Frontend\\controllers\\',
 	'pagination_limit' => 10,
 ];
