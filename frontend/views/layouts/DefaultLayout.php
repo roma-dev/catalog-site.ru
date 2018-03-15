b6245e11ec91eb226dca7182fd87f729fbb081c4
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-15 13:40:39 +0300

    Создание простого html макета для фронтенда.

---
+++ frontend/views/layouts/DefaultLayout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= isset($title) ? $title : 'Catalog-site.ru' ?></title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<script src="/js/scripts.js"></script>
+</head>
+<body>
+	
+<?= $content ?>
+
+<script>
+</script>
+</body>
+</html>
+
