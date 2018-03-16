df462c343f0e3d8d128ec5094b1c28a14aef85b8
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 19:01:55 +0300

    Добавил метод update() в класс Mysql для обновления строк бд.

--- engine/Mysql.php
+++ engine/Mysql.php
@@ -128,4 +128,40 @@ class Mysql extends MysqlConnect{
 			Catalog::$app->httpHeader->error(500, $error->getMessage());
         }
 	}
+	
+	
+	public function update($sql = null)
+	{
+		// проверка переданного значения и свойства объекта билдера
+		if($sql === null) {$sql = $this->sqlBuilder->sql();}
+		if(!$sql){ return false; }
+		
+		try 
+		{
+			// открытие транзакции
+			$this->connection->beginTransaction();
+		
+			// отправляем запрос в бд	
+			$countRows = $this->connection->exec($sql);
+			
+			// в случае успешного исхода фиксируем транзакцию
+			$this->connection->commit();
+			
+			//обнуляем свойство
+			$this->sqlBuilder->sql(null);
+			
+			return $countRows;
+        } 
+		catch (PDOException $error) 
+		{
+			// в случае возникновения ошибки откатываем изменения
+            $this->connection->rollBack();
+			
+			Catalog::$app->httpHeader->error(500, $error->getMessage());
+        }
+		
+	}
+	
+	
+	
 }
