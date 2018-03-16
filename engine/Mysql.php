845ed6d874efd17ec40c9f22106b105e2c9e6054
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 18:53:43 +0300

    Добавил методы select() и selectOne() в класс Mysql для выборки из бд.

--- engine/Mysql.php
+++ engine/Mysql.php
@@ -2,6 +2,9 @@
 
 namespace Engine;
 
+use PDO;
+use PDOException;
+
 class Mysql extends MysqlConnect{
 	
 	private $sqlBuilder;
@@ -45,12 +48,84 @@ class Mysql extends MysqlConnect{
 			// в случае возникновения ошибки откатываем изменения
             $this->connection->rollBack();
 			
-			
 			Catalog::$app->httpHeader->error(500, $error->getMessage());
         }
 		
 	}
 	
 	
+	public function select($sql = null)
+	{
+		// проверка переданного значения и свойства объекта билдера
+		if($sql === null) {$sql = $this->sqlBuilder->sql();}
+		if(!$sql){ return false; }
+		
+		try 
+		{
+			// открытие транзакции
+			$this->connection->beginTransaction();
+		
+			// отправляем запрос в бд и получаем объект PDOStatement
+			$statement = $this->connection->query($sql, PDO::FETCH_ASSOC);
+			
+			// получаем массив для вывода
+			$retrunArray = $statement->fetchAll();
+			
+			// в случае успешного исхода фиксируем транзакцию
+			$this->connection->commit();
+			
+			//обнуляем свойство
+			$this->sqlBuilder->sql(null);
+			
+			return $retrunArray;
+        } 
+		catch (PDOException $error) 
+		{
+			// в случае возникновения ошибки откатываем изменения
+            $this->connection->rollBack();
+			
+			Catalog::$app->httpHeader->error(500, $error->getMessage());
+        }
+	}
+
+	/**
+	 * 
+	 * @param type $sql
+	 * @return boolean false если не найдет строки
+	 */
 	
+	
+	public function selectOne($sql = null)
+	{
+		// проверка переданного значения и свойства объекта билдера
+		if($sql === null) {$sql = $this->sqlBuilder->sql();}
+		if(!$sql){ return false; }
+		
+		try 
+		{
+			// открытие транзакции
+			$this->connection->beginTransaction();
+		
+			// отправляем запрос в бд и получаем объект PDOStatement
+			$statement = $this->connection->query($sql, PDO::FETCH_ASSOC);
+			
+			// получаем массив для вывода
+			$retrunArray = $statement->fetch();
+			
+			// в случае успешного исхода фиксируем транзакцию
+			$this->connection->commit();
+			
+			//обнуляем свойство
+			$this->sqlBuilder->sql(null);
+			
+			return $retrunArray;
+        } 
+		catch (PDOException $error) 
+		{
+			// в случае возникновения ошибки откатываем изменения
+            $this->connection->rollBack();
+			
+			Catalog::$app->httpHeader->error(500, $error->getMessage());
+        }
+	}
 }
