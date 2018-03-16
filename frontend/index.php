93f4c32746b7ae99818de59017ec2c071fc857f5
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 10:13:01 +0300

    Коммит перед реализацией работы с бд.

--- frontend/index.php
+++ frontend/index.php
@@ -8,8 +8,8 @@ define('ROOT_DIR', substr(__DIR__, 0, -8 ));
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$config = require_once __DIR__. '/config/config.php';
-$rules = require_once __DIR__. '/config/rules.php';
+$config		= require_once __DIR__. '/config/config.php';
+$rules		= require_once __DIR__. '/config/rules.php';
 
 Catalog::init($config);
 
