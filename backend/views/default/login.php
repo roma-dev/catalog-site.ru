070eccafd6a398b036c1ca77c3581a33e8cf23fe
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 22:41:12 +0300

    Одолжил у бустрапа вид страницы логина.

--- backend/views/default/login.php
+++ backend/views/default/login.php
@@ -2,3 +2,14 @@
 $title = 'Страница авторизации'; 
 $pagination = false;
 ?>
+
+<script>
+<?php if(isset($error)){ echo "alert('$error');";}?>	
+</script>
+
+<form class="form-signin" role="form" action="/admin/login" method="post">
+	<h2 class="form-signin-heading">Авторизация</h2>
+	<input name="name" type="text" class="form-control" placeholder="Введите ваше имя" required autofocus>
+	<input name="password" type="password" class="form-control" placeholder="Введите ваш пароль" required>
+	<button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+</form>
