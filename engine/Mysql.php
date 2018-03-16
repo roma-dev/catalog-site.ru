401d7dec8fc09eab0f31b8fe913393b7fb7fdbb3
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 19:05:33 +0300

    Добавил метод delete() в класс Mysql для удаления строк бд.

--- engine/Mysql.php
+++ engine/Mysql.php
@@ -129,6 +129,11 @@ class Mysql extends MysqlConnect{
         }
 	}
 	
+	/**
+	 * 
+	 * @param type $sql
+	 * @return int
+	 */
 	
 	public function update($sql = null)
 	{
@@ -163,5 +168,36 @@ class Mysql extends MysqlConnect{
 	}
 	
 	
+	public function delete($sql = null)
+	{
+		// проверка переданного значения и свойства объекта билдера
+		if($sql === null) {$sql = $this->sqlBuilder->sql();}
+		if(!$sql){ return false; }
+		
+		try 
+		{
+			// открытие транзакции
+			$this->connection->beginTransaction();
+		
+			// отправляем запрос в бд	
+			$countRows = $this->connection->exec($sql);
+			
+			// в случае успешного исхода фиксируем транзакцию
+			$this->connection->commit();
+			
+			//обнуляем свойство
+			$this->sqlBuilder->sql(null);
+			
+			return $countRows;
+        } 
+		catch (PDOException $error) 
+		{
+			// в случае возникновения ошибки откатываем изменения
+            $this->connection->rollBack();
+			
+			Catalog::$app->httpHeader->error(500, $error->getMessage());
+        }
+		
+	}
 	
 }
