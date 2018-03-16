a582c0872052533ee2ec7bce46a01b0a7b7ba2f7
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 22:02:27 +0300

    Добавил макет LoginLayouts.php для рендеринга страницы логина.

---
+++ backend/views/layouts/LoginLayout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?= isset($title) ? $title : 'Catalog-site.ru' ?></title>
+	<link rel="stylesheet" type="text/css" href="/admin/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/admin/css/admin.css">
+	<script src="/admin/js/jquery-3.3.1.min.js"></script>
+	<script src="/admin/js/bootstrap.min.js"></script>
+	<script src="/admin/js/admin.js"></script>
+</head>
+<body>
+	<div class="container">
+		
+		<?= $content ?>
+
+	</div>
+
+	</div>
+</body>
+</html>
