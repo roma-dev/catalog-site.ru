febc1f6a6514faf349e7e9dc40a1f05235cfa823
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 23:01:45 +0300

    Включил faceControl в точке входа в админку. Защита админки реализована. Осталось отредактировал макеты, чтоб отображать кнопку выхода, когда админ залогинен.

--- backend/admin.php
+++ backend/admin.php
@@ -5,6 +5,7 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 use Engine\Catalog;
+use Engine\Mysql;
 use Engine\Request;
 
 // минусуем из строки подстроку backend
@@ -12,13 +13,18 @@ define('ROOT_DIR', substr(__DIR__, 0, -7 ));
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$config = require_once __DIR__. '/config/config.php';
-$rules = require_once __DIR__. '/config/rules.php';
+$config		= require_once __DIR__. '/config/config.php';
+$rules		= require_once __DIR__. '/config/rules.php';
+$config_db	= require_once __DIR__. '/config/db.php';
 
 Catalog::init($config);
+
+Catalog::$app->accessControl->faceControl();
 
 Catalog::$app->rules = $rules;
 
 Catalog::$app->request = new Request();
+
+Catalog::$app->db = new Mysql($config_db);
 
 Catalog::$app->goAction();
