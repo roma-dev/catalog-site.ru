3b85d6090328f591f39e76db077c951eac640124
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 19:48:56 +0300

    Коммит

--- frontend/index.php
+++ frontend/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Engine\Catalog;
+use Engine\Mysql;
 use Engine\Request;
 
 // минусуем из строки подстроку frontend
@@ -10,11 +11,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $config		= require_once __DIR__. '/config/config.php';
 $rules		= require_once __DIR__. '/config/rules.php';
+$config_db		= require_once __DIR__. '/config/db.php';
 
 Catalog::init($config);
 
 Catalog::$app->rules = $rules;
 
 Catalog::$app->request = new Request();
+
+Catalog::$app->db = new Mysql($config_db);
 
 Catalog::$app->goAction();
