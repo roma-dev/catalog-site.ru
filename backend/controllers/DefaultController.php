ae7ab48782336b96e6c4c160584871276a9639df
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-16 22:42:43 +0300

    Реализовал экшен логин в контроллере DefaultController. Осталось реализовать логин админа в сессия при успешном логине.

--- backend/controllers/DefaultController.php
+++ backend/controllers/DefaultController.php
@@ -12,20 +12,42 @@ class DefaultController {
 		return Catalog::$app->view->render('dashboard', ['id'=> $id, 'page' => $page]);
 	}
 
-	public function login($id, $page)
+	public function login()
 	{
-		$user = new User();
-		// если пользователя не нашлось в таблице user
-		if( !$user->selectUser('administrator') ) 
+		// переназначаем макет для рендеринга страницы логин
+		Catalog::$app->settings = ['layoutView', 'LoginLayout' ];
+		
+		// если нет пост данных рендерим страницу
+		if(!isset($_POST['name']) && !isset($_POST['password'])) 
 		{
-			return Catalog::$app->view->render('login', ['id'=> $id, 'page' => $page]);
+			return Catalog::$app->view->render('login');
 		}
 		
-		var_dump($user->name); die;
+		$user = new User();
 		
+		// если пользователя не нашлось в таблице user. рендерим. и выводим сообщение об ошибке.
+		if( !$user->selectUser(['name', $_POST['name']]) ) 
+		{
+			return Catalog::$app->view->render('login', ['error' => 'Пользователя с таким именем не найдено!']);
+		}
+		
+		// Проверяем на правильность введеный пароль, если неверный то выводим сообщение
+		if($user->password != $_POST['password'])
+		{
+			return Catalog::$app->view->render('login', ['error' => 'Введен неверный пароль!']);
+		}
+		
+		// проверяем роль посетителя, если у него права админа то логиним его
+		if($user->role == 'admin')
+		{
+			echo 'Данные верны! Вы попали в админку'; die;
+		}
+		
+		// если у пользователя нет прав админа то рендерим с выводом ошибки
+		return Catalog::$app->view->render('login', ['error' => 'У вас нет достаточно прав для входа в админку!']);
 	}
 
-	public function logout($id, $page)
+	public function logout()
 	{
 		return Catalog::$app->view->render('logout', ['id'=> $id, 'page' => $page]);
 	}
