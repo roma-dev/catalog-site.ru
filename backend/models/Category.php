4435bd61ac65792fa0cf299b4065c73805f1fd3d
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-17 14:16:47 +0300

    Создал модель для общения с таблицей category. Создал метод в модели selectCategories для выборки всех категорий с учетом поисковых параметров и pagination_limit из файлв конфига.

---
+++ backend/models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Backend\models;
+
+use Engine\Catalog;
+
+class Category {
+	
+	private $tableName = 'category';
+	
+	private $searchColumns =  [ 'name', 'short_description', 'active' ];
+			
+	
+	public function selectCategories()
+	{
+		// формирует sql для выборки
+		// берутся параметры из гет запроса 
+		Catalog::$app->db->sqlBuilder()
+			->select('*')
+			->from($this->tableName)
+			->orWhereParams(Catalog::$app->request->get, $this->searchColumns)
+			->orderBy('id ASC')
+			->limitPagination()
+			->forSelect();
+		
+		$result = Catalog::$app->db->select();
+		
+		if(!is_array($result) && empty($result)) return false;
+		
+		return $result;
+	}
+	
+	public function __get($name) {
+		return $this->$name;
+	}
+}
