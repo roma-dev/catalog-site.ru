7646fa5315e84e4dcd90b2b095f9e190fe78f6ed
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-17 21:27:33 +0300

    Реализовал вид view для отображения всех данных одной категории в админке.

--- backend/views/category/view.php
+++ backend/views/category/view.php
@@ -1,4 +1,19 @@
 <?php 
-$title = 'Категория'; 
+$title = 'Категория: '.$category['name']; 
 $pagination = false;
 ?>
+<div class="row">
+	<div class="col-lg-9">
+		<dl class="dl-horizontal">
+			<dt class="view-dt-dd">ID Категории:</dt>		<dd class="view-dt-dd"><?=$category['id']?></dd>
+			<dt class="view-dt-dd">Название категории:</dt>	<dd class="view-dt-dd"><?=$category['name']?></dd>
+			<dt class="view-dt-dd">Краткое описание:</dt>	<dd class="view-dt-dd"><?=$category['short_description']?></dd>
+			<dt class="view-dt-dd">Полное описание:</dt>	<dd class="view-dt-dd"><?=$category['full_description']?></dd>
+			<dt class="view-dt-dd">Статус:</dt>				<dd class="view-dt-dd"><?= $category['active'] ? 'Активный': 'Неактивный'?></dd>
+		</dl>
+
+		<div class="row text-right">
+			<a href="/admin/category/update?<?=$category['id']?>" class="btn btn-success">Редактировать</a>
+		</div>
+	</div>
+</div>
