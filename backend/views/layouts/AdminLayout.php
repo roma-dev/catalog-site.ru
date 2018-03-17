d99e477e8779cf5cc1510df5a8f5575f57f7db12
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-17 17:04:32 +0300

    Изменил шаблон макета для админки. Спустил вниз к тегу боду вставки скриптов.

--- backend/views/layouts/AdminLayout.php
+++ backend/views/layouts/AdminLayout.php
@@ -6,9 +6,6 @@
 	<link rel="stylesheet" type="text/css" href="/admin/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/admin/css/dashboard.css">
 	<link rel="stylesheet" type="text/css" href="/admin/css/admin.css">
-	<script src="/admin/js/jquery-3.3.1.min.js"></script>
-	<script src="/admin/js/bootstrap.min.js"></script>
-	<script src="/admin/js/admin.js"></script>
 </head>
 <body>
 
@@ -58,5 +55,8 @@
         </div>
       </div>
     </div>
+<script src="/admin/js/jquery-3.3.1.min.js"></script>
+<script src="/admin/js/bootstrap.min.js"></script>
+<script src="/admin/js/admin.js"></script>
 </body>
 </html>
