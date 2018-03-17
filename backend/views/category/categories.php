f8974782bc85b96c27edce5d9f99dc877abc68f5
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-17 10:27:14 +0300

    Отредактировал макета шаблона для админки.

--- backend/views/category/categories.php
+++ backend/views/category/categories.php
@@ -1,4 +1,192 @@
 <?php 
 $title = 'Категории'; 
-$pagination = false;
+$pagination = true;
 ?>
+
+		<div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><input type="text" class="form-control" placeholder="Text input"></td>
+                  <td><input type="text" class="form-control" placeholder="Text input"></td>
+                  <td><input type="text" class="form-control" placeholder="Text input"></td>
+                  <td><input type="text" class="form-control" placeholder="Text input"></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>Nulla</td>
+                  <td>quis</td>
+                  <td>sem</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,006</td>
+                  <td>nibh</td>
+                  <td>elementum</td>
+                  <td>imperdiet</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,007</td>
+                  <td>sagittis</td>
+                  <td>ipsum</td>
+                  <td>Praesent</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,008</td>
+                  <td>Fusce</td>
+                  <td>nec</td>
+                  <td>tellus</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,009</td>
+                  <td>augue</td>
+                  <td>semper</td>
+                  <td>porta</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,010</td>
+                  <td>massa</td>
+                  <td>Vestibulum</td>
+                  <td>lacinia</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,011</td>
+                  <td>eget</td>
+                  <td>nulla</td>
+                  <td>Class</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,012</td>
+                  <td>taciti</td>
+                  <td>sociosqu</td>
+                  <td>ad</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,013</td>
+                  <td>torquent</td>
+                  <td>per</td>
+                  <td>conubia</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,014</td>
+                  <td>per</td>
+                  <td>inceptos</td>
+                  <td>himenaeos</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+                <tr>
+                  <td>1,015</td>
+                  <td>sodales</td>
+                  <td>ligula</td>
+                  <td>in</td>
+                  <td class="table-td-icons-block">
+					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  
+				  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
--- backend/views/layouts/AdminLayout.php
+++ backend/views/layouts/AdminLayout.php
@@ -4,40 +4,59 @@
 	<meta charset="utf-8">
 	<title><?= isset($title) ? $title : 'Catalog-site.ru' ?></title>
 	<link rel="stylesheet" type="text/css" href="/admin/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/admin/css/dashboard.css">
 	<link rel="stylesheet" type="text/css" href="/admin/css/admin.css">
 	<script src="/admin/js/jquery-3.3.1.min.js"></script>
 	<script src="/admin/js/bootstrap.min.js"></script>
 	<script src="/admin/js/admin.js"></script>
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<h1><?= isset($title) ? $title : 'Catalog-site.ru' ?></h1>
-		</div>
-		<ol class="breadcrumb">
-			<li><a href="#">Главная</a></li>
-			<li><a href="#">Библиотека</a></li>
-			<li class="active">Данные</li>
-		</ol>
-		<div class="row">
-			
 
-		<?= $content ?>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Catalog-site.ru</a>
+        </div>
+      </div>
+    </div>
 
-		</div>
-	<?php if($pagination): ?>	
-		<div class="row text-right">
-			<ul class="pagination">
-				<li class="disabled"><a href="#">&laquo;</a></li>
-				<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class="disabled"><span>&raquo;</span></li>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar dashboard-nav-sidebar">
+			  <li><a href="/admin">Админ панель</a></li>
+            <li><a  href="/admin/categories">Категории</a><a style="float: right;" href="/admin/category/create"><span class="glyphicon glyphicon-plus"></span></a></li>
+            <li><a  href="/admin/goods">Товары</a><a style="float: right;" href="/admin/good/create"><span class="glyphicon glyphicon-plus"></span></a></li>
+            <li><a  href="/admin/logout">Выход</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header"><?= isset($title) ? $title : 'Catalog-site.ru' ?></h1>
+          
+			<?= $content ?>
+		  
+			<?php if($pagination): ?>	
+				<div class="row text-right">
+					<ul class="pagination">
+						<li class="disabled"><a href="#">&laquo;</a></li>
+						<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+						<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
+						<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
+						<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
+						<li class="disabled"><span>&raquo;</span></li>
 
-			</ul>
-		</div>
-	<?php endif; ?>	
-	</div>
+					</ul>
+				</div>
+			<?php endif; ?>	
+		  
+        </div>
+      </div>
+    </div>
 </body>
 </html>
