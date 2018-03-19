a14a7112ddbb15eba21a9b076817995562a29394
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 11:49:24 +0300

    Создал модель Frontend/CatagoryGood и метод selectIdGood который возвращает id товаров по id категории.

---
+++ frontend/models/CategoryGood.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Frontend\models;
+
+use Engine\Catalog;
+
+class CategoryGood {
+	
+	private $tableName = 'category_goods';
+	
+	public function selectIdGoods($idCategory)
+	{
+		$sql = 'SELECT goods_id FROM '.$this->tableName.' WHERE category_id = '.$idCategory.' ORDER BY goods_id DESC';
+		
+		$result = Catalog::$app->db->select($sql);
+		
+		// если пустой массив выводим false
+		if($result === []) return false;
+		
+		$returnArr = [];
+		
+		foreach ($result as $good)
+		{
+			$returnArr[] = $good['goods_id'];
+		}
+		
+		return $returnArr;
+	}
+			
+	
+}
