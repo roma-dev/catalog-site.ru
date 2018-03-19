73a06d8f4600bee4bc88d5cee99b17c923400873
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 14:16:04 +0300

    Реализовал экшен index GoodController frontend

--- frontend/controllers/GoodController.php
+++ frontend/controllers/GoodController.php
@@ -2,12 +2,29 @@
 
 namespace Frontend\controllers;
 
+use Frontend\models\Category;
 use Engine\Catalog;
+use Frontend\models\Good;
 
 class GoodController {
 	
-	public function index($id, $page)
+	public function index($id)
 	{
-		return Catalog::$app->view->render('good', ['id'=> $id, 'page' => $page]);
+		// если не передан айди категории
+		if(!isset($id)) Catalog::$app->httpHeader->error(404, 'Такой страницы не существует! ');
+		
+		$goodModel = new Good();
+		
+		$good = $goodModel->selectOne($id);
+		
+		// если false значит что категории нет в бд
+		if(!$good) Catalog::$app->httpHeader->error(404, 'Такого товара не существует! ');
+		
+		$modelCategory = new Category();
+		
+		$categories = $modelCategory->selectCategories($id);
+		
+		return Catalog::$app->view->render('good', ['categories' => $categories, 'good' => $good, 'id'=> $id]);
+	
 	}
 }
