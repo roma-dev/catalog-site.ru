91a91e7a809c9830a6e5019efe16f45aac42cbf9
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 11:55:49 +0300

    Реализовал экшен index CategoryController.

--- frontend/controllers/CategoryController.php
+++ frontend/controllers/CategoryController.php
@@ -2,12 +2,29 @@
 
 namespace Frontend\controllers;
 
+use Frontend\models\Category;
 use Engine\Catalog;
+use Frontend\models\Good;
 
 class CategoryController {
 	
 	public function index($id, $page)
 	{
-		return Catalog::$app->view->render('category', ['id'=> $id, 'page' => $page]);
+		// если не передан айди категории
+		if(!isset($id)) Catalog::$app->httpHeader->error(404, 'Такой страницы не существует! ');
+		
+		$categoryModel = new Category();
+		
+		$category = $categoryModel->selectOne($id);
+		
+		// если false значит что категории нет в бд
+		if(!$category) Catalog::$app->httpHeader->error(404, 'Такой категории не существует! ');
+		
+		$model = new Good();
+		
+		// выдаст пустой массив если не будет товаров в категории
+		$goods = $model->selectGoods($id);
+		
+		return Catalog::$app->view->render('category', ['category' => $category, 'goods' => $goods, 'id'=> $id, 'page' => $page]);
 	}
 }
