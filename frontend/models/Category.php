2e54733458d54709a52b0c62a006b6941e6dc637
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 10:07:53 +0300

    Создал модель Frontend/Category and method selectAll().

---
+++ frontend/models/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Frontend\models;
+
+use Engine\Catalog;
+
+class Category {
+	
+	private $tableName = 'category';
+	
+	public function selectAll()
+	{
+		// формирует sql для выборки
+		// берутся параметры из гет запроса 
+		Catalog::$app->db->sqlBuilder()
+			->select('*')
+			->from($this->tableName)
+			->where('active = 1')
+			->orderBy('id DESC')
+			->limitPagination()
+			->forSelect();
+		
+		$result = Catalog::$app->db->select();
+		
+		if(!is_array($result) && empty($result)) return false;
+		
+		$counts = Catalog::$app->db->selectOne(Catalog::$app->db->sqlBuilder()->sqlForCount());
+		
+		// сохраняем количество строк в свойство
+		$this->counts = $counts['rows'];
+		
+		return $result;
+	}
+	
+	
+	
+}
