47d9d51ca53d785f4786a9f38ab77ec13b5aebc3
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 09:00:08 +0300

    Добавил верстки в вид goods для отображения таблицы товаров.

--- backend/views/good/goods.php
+++ backend/views/good/goods.php
@@ -1,190 +1,61 @@
 <?php 
 $title = 'Товары'; 
 $pagination = true;
+		
+$name				=  isset($_GET['name']) ? trim($_GET['name'], '[]' ) : '' ;
+$short_description	=  isset($_GET['short_description']) ? trim($_GET['short_description'], '[]' ) : '' ;
+$active				=  isset($_GET['active']) ? $_GET['active'] : null ;
+$count				=  isset($_GET['count']) ? $_GET['count'] : null ;
+$is_available		=  isset($_GET['is_available']) ? $_GET['is_available'] : null ;
 ?>
-<div class="table-responsive">
+
+		<div class="table-responsive">
             <table class="table table-striped">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th></th>
+                  <th class="col-lg-2">Название</th>
+                  <th class="col-lg-6">Краткое описание</th>
+                  <th class="col-lg-1">Статус</th>
+                  <th class="col-lg-1">На складе</th>
+                  <th class="col-lg-1">Предзаказ</th>
+                  <th class="col-lg-1"></th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td><input type="text" class="form-control" placeholder="Text input"></td>
-                  <td><input type="text" class="form-control" placeholder="Text input"></td>
-                  <td><input type="text" class="form-control" placeholder="Text input"></td>
-                  <td><input type="text" class="form-control" placeholder="Text input"></td>
+					<td><input value="<?= $name ? $name:'';?>" id="search-name" name="name" type="text" class="form-control search-form"></td>
+                  <td><input value="<?= $short_description ? $short_description:'';?>" id="search-short_description" name="short_description" type="text" class="form-control search-form"></td>
+                  <td>
+					  <select id="search-active" name="active" type="text" class="form-control search-form">
+						<option></option>
+						<option value="1" <?= ($active == '1')? 'selected':'';?>>Активные</option>
+						<option value="0" <?= ($active == '0')? 'selected':'';?>>Неактивные</option>
+					  </select>
+				  </td>
+                  <td><input value="<?= $count ? $count:'';?>" id="search-count" name="count" type="text" class="form-control search-form"></td>
+                  <td>
+					  <select id="search-is_available" name="is_available" type="text" class="form-control search-form">
+						<option></option>
+						<option value="1" <?= ($is_available == '1')? 'selected':'';?>>Доступеные</option>
+						<option value="0" <?= ($is_available == '0')? 'selected':'';?>>Недоступные</option>
+					  </select>
+				  </td>
                   <td></td>
                 </tr>
+				<?php foreach($result as $row):?>
                 <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
+                  <td><span><?=$row['name']?></span></td>
+                  <td><span><?=$row['short_description']?></span></td>
+                  <td><span><?=$row['active'] ? "Активная" : "Неактивная";?></span></td>
+                  <td><span><?=$row['count']?></span></td>
+				  <td><span><?=$row['is_available'] ? "Доступен" : "Недоступен";?></span></td>
                   <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
+					  <a href="/admin/category/view?id=<?=$row['id']?>"><span class="glyphicon glyphicon-eye-open"></span></a>
+					  <a href="/admin/category/update?id=<?=$row['id']?>"><span class="glyphicon glyphicon-pencil"></span></a>
 					  
 				  </td>
                 </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-				  </td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td class="table-td-icons-block">
-					  <a href="/admin/good/view"><span class="glyphicon glyphicon-eye-open"></span></a>
-					  <a href="/admin/good/update"><span class="glyphicon glyphicon-pencil"></span></a>
-					  
-				  </td>
-                </tr>
+				<?php endforeach; ?>
               </tbody>
             </table>
           </div>
