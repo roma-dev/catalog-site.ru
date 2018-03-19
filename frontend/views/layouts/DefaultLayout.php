b9653b1127cef2270e26394a010d5b33446d5bc4
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 10:08:35 +0300

    Отредактировал макет шаблона DefaultLayout.

--- frontend/views/layouts/DefaultLayout.php
+++ frontend/views/layouts/DefaultLayout.php
@@ -1,17 +1,71 @@
- <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<title><?= isset($title) ? $title : 'Catalog-site.ru' ?></title>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	<script src="/js/scripts.js"></script>
-</head>
-<body>
-   
+    <!-- Custom styles for this template -->
 
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
- 	<script src="/js/jquery-3.3.1.min.js"></script>
-	<script src="/js/bootstrap.min.js"></script>
-  </body>
-</html>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Catalog-site.ru</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1><?= isset($title) ? $title : 'Catalog-site.ru' ?></h1>
+        <p><?= isset($seotext) ? $seotext : '' ?></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+		  
+		  <?=$content?>
+		  
+      </div>
+
+      <hr>
+	  <div class="row text-right">
+	  <ul class="pagination">
+		<li class=""><a href="#">1 <span class="sr-only"></span></a></li>
+		<li class=""><a href="#">1 <span class="sr-only"></span></a></li>
+		<li class="active"><a href="#">1 <span class="sr-only"></span></a></li>
+		<li class=""><a href="#">1 <span class="sr-only"></span></a></li>
+		<li class=""><a href="#">1 <span class="sr-only"></span></a></li>
+	  </ul>
+	  </div>
+	  
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->
