c0664960f5bf3c98607d16c2343da5f8ef589428
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 09:24:15 +0300

    Коммит перед реализацией отображения фронтенда.

--- frontend/views/layouts/DefaultLayout.php
+++ frontend/views/layouts/DefaultLayout.php
@@ -1,44 +1,17 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title><?= isset($title) ? $title : 'Catalog-site.ru' ?></title>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
-	<script src="/js/jquery-3.3.1.min.js"></script>
-	<script src="/js/bootstrap.min.js"></script>
 	<script src="/js/scripts.js"></script>
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<h1><?= isset($title) ? $title : 'Catalog-site.ru' ?></h1>
-		</div>
-		<ol class="breadcrumb">
-			<li><a href="#">Главная</a></li>
-			<li><a href="#">Библиотека</a></li>
-			<li class="active">Данные</li>
-		</ol>
-		<div class="row">
-			
+   
 
-		<?= $content ?>
 
-		</div>
-	<?php if($pagination): ?>	
-		<div class="row text-right">
-			<ul class="pagination">
-				<li class="disabled"><a href="#">&laquo;</a></li>
-				<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class=""><a href="#">1 <span class="sr-only">(current)</span></a></li>
-				<li class="disabled"><span>&raquo;</span></li>
-
-			</ul>
-		</div>
-	<?php endif; ?>	
-	</div>
-</body>
+ 	<script src="/js/jquery-3.3.1.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+  </body>
 </html>
-
