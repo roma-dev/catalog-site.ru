17c0ae595c7ebf471c4caa3ea0013bcce4a7f566
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 14:53:25 +0300

    Подправил вид index frontend. Чтоб категории отображались по 3 в ряд.

--- frontend/views/default/index.php
+++ frontend/views/default/index.php
@@ -3,12 +3,18 @@
 $title = 'Catalog-site.ru';
 $seotext = "Добро пожаловать на наш сайт! <br>У нас вы можете <strong>купить товары в Смоленске по доступной цене</strong> с доставкой на дом.";
 $pagination = true;
-?>
 
-<?php foreach ($categories as $category):?>
-<div class="col-md-4">
-  <h2><?=$category['name']?></h2>
-  <p><?=$category['short_description']?></p>
-  <p><a class="btn btn-success" href="/category?id=<?=$category['id']?>">Подробнее</a></p>
-</div>
-<?php endforeach;?>
+for($i = 0, $j = 1; $i < count($categories); $i++, $j++)
+{
+	if($j == 1 ) echo '<div class="row">';
+	
+	echo '<div class="col-md-4"><h2>'.$categories[$i]['name'].'</h2>';
+	echo '<p>'.$categories[$i]['short_description'].'</p>';
+	echo '<p><a class="btn btn-success" href="/category?id='.$categories[$i]['id'].'">Подробнее</a></p></div>';
+	
+	if($j == 3 ){
+		echo '</div>';
+		$j=0;
+	}
+}
+
