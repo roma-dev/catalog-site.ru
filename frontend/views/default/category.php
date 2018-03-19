849c02d0b3b78a80ab355ce23a6d6f767016d031
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-19 11:53:39 +0300

    Реализовал вид category для отображения товаров категории.

--- frontend/views/default/category.php
+++ frontend/views/default/category.php
@@ -1,4 +1,33 @@
 <?php 
-$title = 'Категория'; 
-$pagination = true;
+	if(isset($category))
+	{
+		$title = 'Категория '. $category['name'];
+		$seotext = $category['full_description'];
+	}else
+	{
+		$title = 'Catalog-site.ru';
+		$seotext = '';
+	}
+	$pagination = true;
 ?>	
+<?php if($goods !== []):?>
+	<?php foreach ($goods as $good):?>
+	<div class="col-md-4">
+	  <h2><?=$good['name']?></h2>
+	  <p><?=$good['short_description']?></p>
+	  <?php
+	  if($good['count'] == 0)
+	  {
+		  echo "<p class=\"text-danger\">Нет в наличии</p>";
+		  if( $good['is_available'] ) echo '<p>Доступен по предзаказу</p>';
+	  }else
+	  {
+		  echo "<p>Количество на скаде: " . $good['count'] . " шт.</p>";
+	  }
+	  ?>
+	  <p><a class="btn btn-success" href="/good?id=<?=$good['id']?>">Подробнее</a></p>
+	</div>
+	<?php endforeach;?>
+<?php else:?>
+	<h3>В категории нет товаров!</h3>
+<?php endif;?>
